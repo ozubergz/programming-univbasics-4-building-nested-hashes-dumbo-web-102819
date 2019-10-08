828807ad80378b97fffe2990602caceea289b0f2
James Min <jmin499@gmail.com>
2019-10-08 00:58:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,5 +50,5 @@ def monopoly_with_fourth_tier
   
   new_hash = monopoly_with_third_tier
   
-  new_hash[:railroads][:names][:reading_railroad] = {"mortgage_value": "$100"}
+  new_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
 end
